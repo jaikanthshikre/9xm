986b4f9999fdb1d870c10f43293baebbf1d26e96
jaikanthshikre <jaikanthshikre36@gmail.com>
2025-08-28 17:40:37 +0530

    finalllll

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* turbopack: { disabled: true }, */
-  output: 'export',
+
+
   images: { unoptimized: true }, // needed for static export if you use next/image
   // optional:
   // trailingSlash: true,
